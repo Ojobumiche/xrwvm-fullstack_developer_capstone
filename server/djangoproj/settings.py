748647121f787e5aa6a2cd0193866acc8dc1c1aa
Ojobumiche <bumiche@gmail.com>
2024-05-31 04:13:55 +0100

    Localhost IP address set up

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "https://127.0.0.1:8000"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ["https://127.0.0.1:8000"]
 
 REST_FRAMEWORK = {
